74ee3ffc2635ff5cc9622386b51874516e77491d
Alexander Boriskin <iboriskin5@gmail.com>
2023-08-31 17:09:44 +0300

    Path corrected

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import csv
 import re
 
-with open('regexp_hw/phonebook_raw.csv', encoding='utf-8') as f:
+with open('phonebook_raw.csv', encoding='utf-8') as f:
     rows = csv.reader(f, delimiter=',')
     contacts_list = list(rows)
 
